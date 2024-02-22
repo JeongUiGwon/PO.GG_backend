da1199ac859fe3b118051d0d185b968abb9bd50a
admin <jek9412@naver.com>
2024-02-22 10:02:58 +0900

    feat: Java Spring 시작 Java Spring 시작

---
+++ lolrecordservice/src/main/java/co/kr/lolrecordservice/LolrecordserviceApplication.java
@@ -0,0 +1,13 @@
+package co.kr.lolrecordservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolrecordserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolrecordserviceApplication.class, args);
+	}
+
+}
